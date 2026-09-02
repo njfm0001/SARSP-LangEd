2c7879f0c209d189b4416dea47c28e3b2ccc9203
Nicolás José Fernández-Martínez <45042730+njfm0001@users.noreply.github.com>
2026-09-02 16:38:30 +0200

    Add files via upload

--- core/consent.py
+++ core/consent.py
@@ -233,18 +233,7 @@ def render_consent_gate():
     
     if accept_btn and terms_check and privacy_check:
         set_consent(True, True)
-        # Force full page reload to reset scroll position
-        st.markdown(
-            """
-            <script>
-                setTimeout(function() {
-                    window.location.reload();
-                }, 100);
-            </script>
-            """,
-            unsafe_allow_html=True
-        )
-        st.stop()
+        st.rerun()
     
     return False
 
